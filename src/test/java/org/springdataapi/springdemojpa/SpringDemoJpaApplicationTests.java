e7d48f21187ca01efad0712253eaf42c67048733
MrMoebius <e.spadaro.ro@gmail.com>
2026-01-19 12:07:21 +0100

    Fisrt commit

---
+++ src/test/java/org/springdataapi/springdemojpa/SpringDemoJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.springdataapi.springdemojpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDemoJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
