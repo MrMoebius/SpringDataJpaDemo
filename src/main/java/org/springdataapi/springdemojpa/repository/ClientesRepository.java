cc628fe3137a7b00b73ab65eb7b84d39d4ab4f42
DaRoCo7 <drojas11124@gmail.com>
2026-01-22 12:41:40 +0100

    anotaciones puestas

--- src/main/java/org/springdataapi/springdemojpa/repository/ClientesRepository.java
+++ src/main/java/org/springdataapi/springdemojpa/repository/ClientesRepository.java
@@ -1,11 +1,9 @@
 package org.springdataapi.springdemojpa.repository;
 
 import org.springdataapi.springdemojpa.models.Clientes;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.ListCrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
